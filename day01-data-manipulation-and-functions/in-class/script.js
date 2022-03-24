93922612eade30567b11eaaa2f0fa45c3b14f815
HAJIWEE <99461740+HAJIWEE@users.noreply.github.com>
2022-03-24 21:22:42 +0800

    initial commit

--- day01-data-manipulation-and-functions/in-class/script.js
+++ day01-data-manipulation-and-functions/in-class/script.js
@@ -1,35 +1,49 @@
 var fahrenheitToCelsiusMain = function (input) {
   // Complete the Base: Fahrenheit to Celsius exercise below with fahrenheitToCelsiusMain as the main function.
-  var myOutputValue = 'hello world';
+  var celcius = ((input - 32) * 5) / 9;
+  var myOutputValue = celcius + "^C";
   return myOutputValue;
 };
 
 var roadTripCostBaseMain = function (input) {
   // Complete the Base: Road Trip Cost exercise below with roadTripCostBaseMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var roadTripCostComfortableMain = function (input) {
   // Complete the Comfortable: Road Trip Cost exercise below with roadTripCostComfortableMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var iceMachineMain = function (input) {
   // Complete the More Comfortable: Ice Machine exercise below with iceMachineMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var beerOrderMain = function (input) {
+  // input is average number of customers
   // Complete the More Comfortable: Beer Order exercise below with beerOrderMain as the main function.
-  var myOutputValue = 'hello world';
+  var totalPintsPerDay = 2 * input; //total pints per day.(In pints)
+  var totalDaysPerQuarter = 365 / 4; //num of days in a quarter year
+  var totalPintsPerQuarter = totalPintsPerDay * totalDaysPerQuarter;
+  var totalKegsPerDay = totalPintsPerDay / (124 * 2); //kegs per day
+  var halfBarrelSizePerQuarter = totalPintsPerQuarter / 124; //total number of half barrel sized keg
+  var myOutputValue = `Kegs of Beer required per day: ${totalKegsPerDay.toFixed(
+    2
+  )} and ${halfBarrelSizePerQuarter.toFixed(
+    2
+  )} half barrel size kegs per quarter needed`;
   return myOutputValue;
 };
 
 var cellularDataMain = function (input) {
+  //input here is how much GB user use per month
   // Complete the More Comfortable: Cost of Cellular Data exercise below with cellularDataMain as the main function.
-  var myOutputValue = 'hello world';
+  var totalNumOfPlan = Math.ceil(input / 50);
+  var pricePerGb = (19.99 * totalNumOfPlan) / input;
+  var myOutputValue = `Price per Gb paid: $${pricePerGb.toFixed(2)}`;
   return myOutputValue;
 };
--- day01-data-manipulation-and-functions/pre-class/script.js
+++ day01-data-manipulation-and-functions/pre-class/script.js
@@ -1,34 +1,83 @@
+var convertKmToMiles = function (distanceInKm) {
+  var distanceInMiles = distanceInKm * 0.62;
+  return distanceInMiles;
+};
+
+var toCatchUp = function (timeDelayed) {
+  var distance = 200 * 2; //train 1 speed and time taken to reach tokyo
+  var timeLeft = timeDelayed;
+  if (timeDelayed >= 2) {
+    timeLeft = 2 - timeDelayed / 60;
+  } else {
+    timeLeft = 2 - timeDelayed;
+  }
+
+  var newSpeed = distance / timeLeft;
+  return newSpeed;
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  //changed the value of myOutputValue to Hello <space> <input from user>
+  var myOutputValue = "hello " + input;
   return myOutputValue;
 };
 
 var greetingMain = function (input) {
   // Attempt the Greeting Program exercise from the Our First Program module below with greetingMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello " + input + " you look smart today!";
   return myOutputValue;
 };
 
 var metricMain = function (input) {
   // Attempt the Metric Conversion Program exercise from the Our First Program module below with metricMain as the main function.
-  var myOutputValue = 'hello world';
+  var distanceInKm = input;
+  var distanceInMiles = distanceInKm * 0.62;
+  var myOutputValue =
+    "hi, " + input + " kilometers is " + distanceInMiles + " miles.";
   return myOutputValue;
 };
 
 var functionsExampleMain = function (input) {
   // Attempt the Base: Run Example Code exercise from the Functions I module below with functionsExampleMain as the main function.
-  var myOutputValue = 'hello world';
+  var userInput = input;
+  var myOutputValue =
+    "hi, " +
+    input +
+    " kilometers is " +
+    convertKmToMiles(userInput) +
+    " miles.";
   return myOutputValue;
 };
 
 var trainSpeedMain = function (input) {
   // Attempt the Comfortable: Train Speed exercise from the Functions I module below with trainSpeedMain as the main function.
-  var myOutputValue = 'hello world';
+  //Get input from train 2 conductor:
+  var userInput = input;
+  //pass input into function to calculate new speed for train 2:
+  var train2Speed = toCatchUp(userInput);
+  var myOutputValue =
+    "Hi, to catch up to train 1, you need to go at: " + train2Speed + " kph.";
   return myOutputValue;
 };
 
 var clockMain = function (input) {
   // Attempt the More Comfortable: Clock exercise from the Functions I module below with clockMain as the main function.
-  var myOutputValue = 'hello world';
+  //Define the clock and angles in degrees: minute hand moves and hour hand moves every minute.
+  var hourHand = 1.0;
+  var minuteHand = input;
+  for (minuteHand; minuteHand >= 60.0; minuteHand -= 60.0) {
+    hourHand += 1.0;
+  }
+  hourHand = hourHand + minuteHand / 60.0;
+  var hourHandDeg = (360.0 / 12.0) * hourHand;
+  var minuteHandDeg = (360.0 / (12.0 * 60.0)) * minuteHand;
+  var degDiff = Math.abs(minuteHandDeg - hourHandDeg);
+  var degDiffAlt = 360.0 - degDiff;
+  var myOutputValue =
+    "Angle between hour hand and minute hand at given time is: " +
+    degDiff +
+    " degrees or " +
+    degDiffAlt +
+    " degrees.";
   return myOutputValue;
 };
