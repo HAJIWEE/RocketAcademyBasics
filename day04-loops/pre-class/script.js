0357db02576f5f6d566322c6edd44b7125deae6f
HAJIWEE <99461740+HAJIWEE@users.noreply.github.com>
2022-04-04 19:52:53 +0800

    initial commit

--- day04-loops/pre-class/script.js
+++ day04-loops/pre-class/script.js
@@ -1,23 +1,59 @@
 var followAlongMain = function (input) {
   // Attempt the Follow Along exercise from the Loops module below with followAlongMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+  var counter = 0;
+
+  // Continue the loop while counter is less than the input value
+  while (counter < input) {
+    // If counter is less than 5, add "yes" to output
+    if (counter < 5) {
+      myOutputValue = myOutputValue + "yes";
+      // Otherwise, add "no" to output
+    } else {
+      myOutputValue = myOutputValue + "no";
+    }
+    counter = counter + 1;
+  }
   return myOutputValue;
 };
-
 var simpleLoopMain = function (input) {
   // Attempt the Simple Loop with Variations exercise from the Loops module below with simpleLoopMain as the main function.
-  var myOutputValue = 'hello world';
+  var counter = 0;
+  var myOutputValue = "";
+  while (counter < 6) {
+    myOutputValue = myOutputValue + "hello ";
+    counter++;
+  }
   return myOutputValue;
+  //if counter starts as a number other than zero it will loop 5 - counter times
+  //if we add more than 1 to counter in the while loop, the loop will loop 5/number added times
+  //it will loop 7 times not 6.
 };
 
 var loopWithinLoopMain = function (input) {
   // Attempt the Loop Within Loop exercise from the Loops module below with loopWithinLoopMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+  var inCounter = 0;
+  var outCounter = 0;
+
+  while (outCounter < 3) {
+    while (inCounter < 3) {
+      myOutputValue = myOutputValue + "hello";
+      inCounter++;
+    }
+    myOutputValue = myOutputValue + "</br>";
+    outCounter++;
+    inCounter = 0;
+  }
   return myOutputValue;
 };
 
 var infiniteLoopMain = function (input) {
   // Attempt the Infinite Loop exercise from the Loops module below with infiniteLoopMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
+  var counter = true;
+  while (counter == true) {
+    console.log(myOutputValue);
+  }
   return myOutputValue;
 };
--- project1-sps/script.js
+++ project1-sps/script.js
@@ -1,4 +1,72 @@
+var userWin = 0;
+var comWin = 0;
+var draws = 0;
+var mode = true;
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "Input scissors, paper or stone"; //user input error default output
+  var randomDigit = Math.floor(Math.random() * 3);
+  var comChoice = "";
+  switch (randomDigit) {
+    case 1:
+      comChoice = "scissors";
+      break;
+    case 2:
+      comChoice = "paper";
+      break;
+    default:
+      comChoice = "stone";
+      break;
+  }
+
+  if (mode == true) {
+    if (input == comChoice) {
+      //check draw conditions
+      myOutputValue = `ITS A DRAW! </br>input scissors, paper or stone to continue. </br>You won: ${userWin}</br>Computer won: ${comWin}</br>Draws : ${draws}`;
+      draws += 1;
+    } else if (
+      //check losing conditions
+      (input == "scissors" && comChoice == "stone") ||
+      (input == "stone" && comChoice == "paper") ||
+      (input == "paper" && comChoice == "scissors")
+    ) {
+      comWin += 1;
+      myOutputValue = `Computer chose ${comChoice}, you lose! </br>Input scissors, paper or stone to continue.</br>You won: ${userWin}</br>Computer won: ${comWin}</br>Draws : ${draws}`;
+    } else if (
+      //check winning conditions
+      (input == "scissors" && comChoice == "paper") ||
+      (input == "stone" && comChoice == "scissors") ||
+      (input == "paper" && comChoice == "stone")
+    ) {
+      userWin += 1;
+      myOutputValue = `Computer chose ${comChoice}, you win! </br>Input scissors, paper or stone to continue.</br>You won: ${userWin}</br>Computer won: ${comWin}</br>Draws : ${draws}`;
+    } else if (input == "reverse") {
+      mode = !mode;
+    }
+  } else {
+    if (input == comChoice) {
+      //check draw conditions
+      myOutputValue = `ITS A DRAW! </br>input scissors, paper or stone to continue. </br>You won: ${userWin}</br>Computer won: ${comWin}</br>Draws : ${draws}`;
+      draws += 1;
+    } else if (
+      //check losing conditions
+      (input == "scissors" && comChoice == "stone") ||
+      (input == "stone" && comChoice == "paper") ||
+      (input == "paper" && comChoice == "scissors")
+    ) {
+      userWin += 1;
+      myOutputValue = `Computer chose ${comChoice}, you win! </br>Input scissors, paper or stone to continue.</br>You won: ${userWin}</br>Computer won: ${comWin}</br>Draws : ${draws}`;
+    } else if (
+      //check winning conditions
+      (input == "scissors" && comChoice == "paper") ||
+      (input == "stone" && comChoice == "scissors") ||
+      (input == "paper" && comChoice == "stone")
+    ) {
+      comWin += 1;
+      myOutputValue = `Computer chose ${comChoice}, you lose! </br>Input scissors, paper or stone to continue.</br>You won: ${userWin}</br>Computer won: ${comWin}</br>Draws : ${draws}`;
+    } else if (input == "reverse") {
+      mode = !mode;
+    }
+  }
   return myOutputValue;
 };
