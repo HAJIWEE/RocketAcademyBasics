d5e1a667be4741be60cd24b68b1be1770020f5c2
HAJIWEE <99461740+HAJIWEE@users.noreply.github.com>
2022-04-18 23:08:07 +0800

    blackjack base done

--- project3-blackjack/script.js
+++ project3-blackjack/script.js
@@ -1,3 +1,53 @@
+var buttons = document.querySelector("#buttons");
+var button1 = document.querySelector("#button1");
+
+var button2 = document.createElement("button");
+button2.id = "button2";
+button2.innerText = "stand";
+
+var button3 = document.createElement("button");
+button3.innerText = "run";
+button3.id = "button3";
+
+button1.addEventListener("click", function () {
+  var result = hit();
+  var output = document.querySelector("#output-div");
+  output.innerHTML = result;
+});
+
+button2.addEventListener("click", function () {
+  var result = stand();
+  var output = document.querySelector("#output-div");
+  output.innerHTML = result;
+});
+
+button3.addEventListener("click", function () {
+  var result = run();
+  var output = document.querySelector("#output-div");
+  output.innerHTML = result;
+});
+
+var comHand = [];
+var playerHand = [];
+var mode = 0;
+var playerRank = 0;
+var comRank = 0;
+
+var end = function () {
+  playerHand = [];
+  comHand = [];
+  button1.innerText = "Start";
+  buttons.removeChild(button2);
+
+  mode = 0;
+};
+
+var run = function () {
+  end();
+  buttons.removeChild(button3);
+  return "Player Runs";
+};
+
 var createDeck = function () {
   // Create non-shuffled deck
   var cardDeck = [];
@@ -58,12 +108,6 @@ var shuffleCards = function (cardDeck) {
   return cardDeck;
 };
 
-var comHand = [];
-var playerHand = [];
-var mode = 0;
-var playerRank = 0;
-var comRank = 0;
-
 var draw = function () {
   for (var i = 0; i < 2; i += 1) {
     playerHand.push(shuffleCards(cardDeck).pop());
@@ -73,27 +117,22 @@ var draw = function () {
   playerRank = checkRankAce(playerHand);
   comRank = checkRankAce(comHand);
 
+  if (playerRank === 21 && comRank !== 21) {
+    end();
+    return "</br>BLACK JACK. Player wins";
+  } else if (comRank === 21 && playerRank !== 21) {
+    end();
+    return ` & ${comHand[1].name} of ${comHand[1].suit}</br>BLACK JACK. computer wins`;
+  } else if (comRank === 21 && playerRank === 21) {
+    end();
+    return ` & ${comHand[1].name} of ${comHand[1].suit}</br>BLACK JACKs. its a TIE!`;
+  }
+
   while (comRank < 17) {
     comHand.push(shuffleCards(cardDeck).pop());
     comRank = checkRankAce(comHand);
   }
 
-  if (playerRank === 21 && comRank !== 21) {
-    playerHand = [];
-    comHand = [];
-    mode = 0;
-    return "</br>BLACK JACK. Player wins";
-  } else if (comRank === 21 && playerRank !== 21) {
-    playerHand = [];
-    comHand = [];
-    mode = 0;
-    return ` & ${comHand[1].name} of ${comHand[1].suit}</br>BLACK JACK. computer wins`;
-  } else if (comRank === 21 && playerRank === 21) {
-    playerHand = [];
-    comHand = [];
-    mode = 0;
-    return ` & ${comHand[1].name} of ${comHand[1].suit}</br>BLACK JACKs. its a TIE!`;
-  }
   return "</br>Click Hit to draw card or Stand to end turn";
 };
 
@@ -117,39 +156,89 @@ var checkRankAce = function (input) {
   return sumOfCard;
 };
 
-var hit = function (input) {
+var winningCondition = function (input) {
+  var myOutputValue = "";
+  var winlose = false;
+
+  if (input === "stand") {
+    winlose = true;
+    if (comRank > 21 && playerRank > 21) {
+      //both explode
+      myOutputValue += "</br>Both lose, ";
+    } else if (comRank > 21 && playerRank <= 21) {
+      //banker explode
+      myOutputValue += `</br>Player WINS,</br>Computer's hand: ${comHand[0].name} of ${comHand[0].suit}`;
+      for (var f = 1; f < comHand.length; f += 1) {
+        myOutputValue += `& ${comHand[f].name} of ${comHand[f].suit}`;
+      }
+      myOutputValue += `</br>`;
+    } else if (playerRank > 21 && comRank <= 21) {
+      //player explode
+      myOutputValue += "</br>Player loses, ";
+    } else if (comRank > playerRank) {
+      //banker nearer to 21
+      myOutputValue += `</br>Player loses,</br>Computer's hand: ${comHand[0].name} of ${comHand[0].suit}`;
+      for (var f = 1; f < comHand.length; f += 1) {
+        myOutputValue += `& ${comHand[f].name} of ${comHand[f].suit}`;
+      }
+    } else if (comRank === playerRank) {
+      //equal but never explode
+      myOutputValue += `</br>Its a TIE,</br>Computer's hand: ${comHand[0].name} of ${comHand[0].suit}`;
+      for (var f = 1; f < comHand.length; f += 1) {
+        myOutputValue += `& ${comHand[f].name} of ${comHand[f].suit}`;
+      }
+    } else if (comRank < playerRank) {
+      //player nearer to 21
+      myOutputValue += `</br>Player WINS,</br>Computer's hand: ${comHand[0].name} of ${comHand[0].suit}`;
+      for (var f = 1; f < comHand.length; f += 1) {
+        myOutputValue += `& ${comHand[f].name} of ${comHand[f].suit}`;
+      }
+    }
+  } else if (input === "hit") {
+    if (playerRank === 21 && comRank !== 21) {
+      //player wins
+      winlose = true;
+      myOutputValue = "</br>21! Player wins";
+    } else if (playerRank > 21 && comRank <= 21) {
+      //player explode
+      winlose = true;
+      myOutputValue += "</br>Player loses";
+    } else if (comRank > 21 && playerRank > 21) {
+      //both explode
+      winlose = true;
+      myOutputValue += "</br>Both lose";
+    }
+  }
+  if (winlose === true) {
+    end();
+  }
+  return myOutputValue;
+};
+
+var hit = function () {
   var myOutputValue = "";
 
   if (mode === 0) {
+    button1.innerText = "Hit";
     mode = 1;
     var result1 = draw();
     myOutputValue =
       `Player's hand: ${playerHand[0].name} of ${playerHand[0].suit} & ${playerHand[1].name} of ${playerHand[1].suit}.
       </br>Computer's hand: HIDDEN & ${comHand[0].name} of ${comHand[0].suit}` +
       result1;
+    buttons.appendChild(button2);
+    if (playerRank === 16) {
+      buttons.appendChild(button3);
+    }
   } else if (mode === 1) {
+    console.log(mode);
     playerHand.push(shuffleCards(cardDeck).pop());
     playerRank = checkRankAce(playerHand);
     myOutputValue = `Player's hand: ${playerHand[0].name} of ${playerHand[0].suit} & ${playerHand[1].name} of ${playerHand[1].suit}`;
     for (var n = 2; n < playerHand.length; n += 1) {
       myOutputValue += ` & ${playerHand[n].name} of ${playerHand[n].suit}`;
     }
-    if (playerRank === 21) {
-      myOutputValue += "</br>Player wins, click any button to start new game";
-      playerHand = [];
-      comHand = [];
-      mode = 0;
-    } else if (playerRank > 21 && comRank <= 21) {
-      myOutputValue += "</br>Player loses, click any button to start new game";
-      playerHand = [];
-      comHand = [];
-      mode = 0;
-    } else if (playerRank > 21 && comRank > 21) {
-      myOutputValue += "</br>Both lose, click any button to start new game";
-      playerHand = [];
-      comHand = [];
-      mode = 0;
-    }
+    myOutputValue += winningCondition("hit");
   }
 
   return myOutputValue;
@@ -166,60 +255,15 @@ var stand = function (input) {
       </br>Computer's hand: HIDDEN & ${comHand[0].name} of ${comHand[0].suit}` +
       result1;
   } else if (mode === 1) {
-    console.log(playerRank);
-    console.log(comRank);
     myOutputValue = `Player's hand: ${playerHand[0].name} of ${playerHand[0].suit} & ${playerHand[1].name} of ${playerHand[1].suit}`;
     for (var n = 2; n < playerHand.length; n += 1) {
       myOutputValue += ` & ${playerHand[n].name} of ${playerHand[n].suit}`;
     }
-    if (comRank > 21 && playerRank <= 21) {
-      myOutputValue += `</br>Player WINS,</br>Computer's hand: ${comHand[0].name} of ${comHand[0].suit}`;
-      for (var f = 1; f < comHand.length; f += 1) {
-        myOutputValue += `& ${comHand[f].name} of ${comHand[f].suit}`;
-      }
-      myOutputValue += `</br>click any button to start new game`;
-      playerHand = [];
-      comHand = [];
-      mode = 0;
-    } else if (playerRank > 21 && comRank <= 21) {
-      myOutputValue += "</br>Player loses, click any button to start new game";
-      playerHand = [];
-      comHand = [];
-      mode = 0;
-    } else if (comRank > playerRank && comRank <= 21) {
-      myOutputValue += `</br>Player loses,</br>Computer's hand: ${comHand[0].name} of ${comHand[0].suit}`;
-      for (var f = 1; f < comHand.length; f += 1) {
-        myOutputValue += `& ${comHand[f].name} of ${comHand[f].suit}`;
-      }
-      myOutputValue += `</br>click any button to start new game`;
-      playerHand = [];
-      comHand = [];
-      mode = 0;
-    } else if (playerRank > 21 && comRank > 21) {
-      myOutputValue += "</br>Both lose, click any button to start new game";
-      playerHand = [];
-      comHand = [];
-      mode = 0;
-    } else if (comRank === playerRank) {
-      myOutputValue += `</br>Its a TIE,</br>Computer's hand: ${comHand[0].name} of ${comHand[0].suit}`;
-      for (var f = 1; f < comHand.length; f += 1) {
-        myOutputValue += `& ${comHand[f].name} of ${comHand[f].suit}`;
-      }
-      myOutputValue += `</br>click any button to start new game`;
-      playerHand = [];
-      comHand = [];
-      mode = 0;
-    } else if (comRank < playerRank && playerRank <= 21) {
-      myOutputValue += `</br>Player WINS,</br>Computer's hand: ${comHand[0].name} of ${comHand[0].suit}`;
-      for (var f = 1; f < comHand.length; f += 1) {
-        myOutputValue += `& ${comHand[f].name} of ${comHand[f].suit}`;
-      }
-      myOutputValue += `</br>click any button to start new game`;
-      playerHand = [];
-      comHand = [];
-      mode = 0;
+    if (playerRank >= 16) {
+      myOutputValue += winningCondition("stand");
+    } else if (playerRank < 16) {
+      myOutputValue = `card value too low please draw`;
     }
   }
-
   return myOutputValue;
 };
